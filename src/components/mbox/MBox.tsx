09e2db3ca77ad19ec53a8c00da541031d59c76d7
Jan Koeppen <jan@yaneq.com>
2024-10-06 21:11:34 -0300

    add map data clusters

--- src/components/mbox/MBox.tsx
+++ src/components/mbox/MBox.tsx
@@ -19,8 +19,71 @@ export const MBox = ({ coords }: { coords: IMapCoords }) => {
     if (mapContainerRef.current) {
       mapRef.current = new mapboxgl.Map({
         container: mapContainerRef.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
         center: [coords.lat, coords.lng],
         zoom: 10.12,
+      });
+      mapRef.current.on('load', () => {
+        mapRef.current?.addSource('wildfires', {
+          type: 'geojson',
+          data: 'http://localhost:3000/brazil.geojson',
+          cluster: true, // Enable clustering
+          clusterMaxZoom: 14, // Max zoom to cluster points
+          clusterRadius: 50, // Radius of each cluster in pixels
+        });
+
+        mapRef.current?.addLayer({
+          id: 'clusters',
+          type: 'circle',
+          source: 'wildfires',
+          filter: ['has', 'point_count'], // Only display clusters
+          paint: {
+            'circle-color': [
+              'step',
+              ['get', 'point_count'],
+              '#e22822', // Color for small clusters
+              100, // Change color for larger clusters
+              '#e85607',
+              750,
+              '#fede17',
+            ],
+            'circle-radius': [
+              'step',
+              ['get', 'point_count'],
+              20, // Radius for small clusters
+              100, // Change radius for larger clusters
+              30,
+              750,
+              40,
+            ],
+          },
+        });
+
+        mapRef.current?.addLayer({
+          id: 'cluster-count',
+          type: 'symbol',
+          source: 'wildfires',
+          filter: ['has', 'point_count'],
+          layout: {
+            'text-field': '{point_count_abbreviated}', // Show the count
+            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+            'text-size': 12,
+          },
+        });
+
+        // Add unclustered points layer
+        mapRef.current?.addLayer({
+          id: 'unclustered-point',
+          type: 'circle',
+          source: 'wildfires',
+          filter: ['!', ['has', 'point_count']], // Only display individual points
+          paint: {
+            'circle-color': '#11b4da',
+            'circle-radius': 4,
+            'circle-stroke-width': 1,
+            'circle-stroke-color': '#fff',
+          },
+        });
       });
     }
 
